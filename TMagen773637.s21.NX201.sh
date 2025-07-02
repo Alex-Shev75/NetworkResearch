fa6a3a58a8d40a4e78ee3535f7ff75d6d2457652
Alex-Shev75 <a.shev4uk1975@gmail.com>
2025-07-02 17:27:43 -0400

    Added check_nipe function and separated functionalities into distinct functions

--- TMagen773637.s21.NX201.sh
+++ TMagen773637.s21.NX201.sh
@@ -19,39 +19,55 @@ START()
         mkdir -p "$working_dir"
         cd "$working_dir"
         echo -e "\e[30m\e[107mCurrent working directory: $(pwd)\e[0m\n"
-        CHECK_APP
+        CHECK_INTERNET_CONNECTION
     fi     
 }
 
-CHECK_APP()
+CHECK_INTERNET_CONNECTION()
 {
     echo -e "\e[31m[*]\e[0m\e[34m Checking for the presence of utilities required for performing the analysis:\e[0m"
     sleep 2
     if ping -c 1  -W 3 8.8.8.8 > /dev/null 2>&1; then
         apt update > /dev/null 2>&1
+        CHECK_NIPE
     else
         echo -e "\n\e[91m\e[107m[!] No internet connection. Check your network.\e[0m\n"
             sleep 2
             exit 1
     fi
+}
 
-    utilites_for_check="nipe tor ssh nmap whois curl"
-    for i in $utilites_for_check; do
+CHECK_NIPE()
+{
+    echo -e "[*] Checking nipe..."
+    nipe_path=$(find / -type f -name nipe.pl 2>/dev/null | grep "/nipe/nipe.pl" | head -n 1)
+    if [[ -z "$nipe_path" ]]; then
+        echo -e "\e[91m\e[107m[!] 'nipe.pl' not found on the system.\e[0m"
+        echo -e "\e[31m[*]\e[0m\e[34m Installing nipe...\e[0m"
+        if [[ ! -d /opt ]]; then
+            mkdir -p /opt
+        fi
+        cd /opt
+        git clone https://github.com/htrgouvea/nipe.git || { echo -e "\e[91m\e[107m[!] Failed to clone nipe.\e[0m"; exit 1; }
+        cd nipe
+        cpan install Try::Tiny Config::Simple JSON || { echo -e "\e[91m\e[107m[!] Failed to install.\e[0m"; exit 1; }
+        perl nipe.pl install || { echo -e "\e[91m\e[107m[!] Failed to install.\e[0m"; exit 1; }
+        echo -e "\e[31m[*]\e[0m\e[32m nipe installed successfully\e[0m"       
+    else
+        echo -e "\e[32m[✔] nipe\e[0m"
+        sleep 0.6
+    fi
+    CHECK_APP
+}
+
+
+CHECK_APP()
+{
+    utilities_for_check="tor ssh nmap whois curl"
+    for i in $utilities_for_check; do
         if ! command -v "$i" > /dev/null 2>&1; then
             echo -e "\e[91m\e[107m[!] '$i' is not installed.\e[0m"
-            if [[ "$i" == "nipe" ]]; then
-                INSTALL_NIPE
-            else
-                apt install "$i" -y > /dev/null 2>install_error.log
-                if [[ $? -eq 0 ]]; then
-                    echo -e "\e[32m[✔] $i installed successfully\e[0m"
-                    # TODO: continue the script
-                else
-                    echo -e "\e[91m\e[107m[!] Failed to install '$i' .\e[0m"
-                    tail -n 5 install_error.log
-                    exit 1
-                fi
-            fi
+            apt install "$i" -y || { echo -e "\e[91m\e[107m[!] Failed to install '$i'.\e[0m"; exit 1; }
         else
             echo -e "\e[32m[✔] $i\e[0m"
         fi
@@ -59,10 +75,5 @@ CHECK_APP()
     done
 }
 
-INSTALL_NIPE()
-{
-    echo -e "[*] Installing nipe..."
-    # TODO: (git clone... etc.)
-}
 
 START
